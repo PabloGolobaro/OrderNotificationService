e04607e8a4d637a55051563d3665fcb754bd9288
PabloGolobaro <yeu334@yandex.ru>
2022-12-08 22:56:07 +0300

    Added order json example

---
+++ models/order_test.go
@@ -0,0 +1,25 @@
+package models
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestOrder(t *testing.T) {
+	order := &Order{
+		Products:  []Product{{Id: 1, Category: "Food", Img: "123.png", Title: "Shashlik", Price: "120", Count: 3}, {Id: 2, Category: "Food", Img: "333.png", Title: "Sauce", Price: "30", Count: 1}},
+		Data:      OrderData{Adress: "Orel, sm. Per 10", Mobile: "89991002345671", Time: time.Now().Format("02.01.2006")},
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+	file, _ := os.Create("order.json")
+	bytes, _ := json.Marshal(order)
+	_, err := file.Write(bytes)
+	if err != nil {
+		log.Panic(err)
+	}
+	return
+}
